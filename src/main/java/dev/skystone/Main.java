6b61e006062b6c1bd249039f8caceeff753ffaaa
Szabo Jozsef <skystone213@gmail.com>
2026-03-23 15:05:50 +0100

    1.feladat megoldva

--- src/main/java/dev/skystone/Main.java
+++ src/main/java/dev/skystone/Main.java
@@ -2,8 +2,8 @@ package dev.skystone;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Járművek feldolgozása...");
         Solution.getVehicleList();
-        Solution.printVehicleBrandList();
+        // Solution.printVehicleBrandList();
+        Solution.printPetrolVehicleDetails();
     }
 }
--- src/main/java/dev/skystone/Solution.java
+++ src/main/java/dev/skystone/Solution.java
@@ -8,9 +8,19 @@ public class Solution {
         vehicleList = Storage.readFile();
     }
 
-    public static void printVehicleBrandList(){
-        vehicleList.forEach(vehicle -> {
-            System.out.println(vehicle.getBrand());
+    // public static void printVehicleBrandList(){
+    //     vehicleList.forEach(vehicle -> {
+    //         System.out.println(vehicle.getBrand());
+    //     });
+    // } 
+    public static void printPetrolVehicleDetails(){
+        System.out.println("Szit.hu 0824-es feladat: 1. feladat");
+        vehicleList.forEach(v -> {
+            if(v.fuel.equals("benzin")){
+                System.out.printf("%s %s %d %s %d %s %s %n",v.getPlate(),v.getBrand(),v.getYear(),
+                v.getFuel(),v.getPrice(),v.isClimate() ? "Van klíma" : "Nincs klíma",v.getPermitDate());
+            };
         });
-    } 
+    }
+
 }
